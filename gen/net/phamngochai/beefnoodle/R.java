af610874efc035d4be9a1620f1c0abad77798c41
ngochai <pngochai@yahoo.com>
2011-04-05 15:45:00 +0800

    code

--- gen/net/phamngochai/beefnoodle/R.java
+++ gen/net/phamngochai/beefnoodle/R.java
@@ -13,11 +13,18 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int Controls=0x7f050000;
+        public static final int Meanning=0x7f050003;
+        public static final int Search=0x7f050002;
+        public static final int Word=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**      <string name="hello">Hello World, MainActivity!</string> 
+         */
+        public static final int app_name=0x7f040000;
     }
 }
--- src/net/phamngochai/beefnoodle/MainActivity.java
+++ src/net/phamngochai/beefnoodle/MainActivity.java
@@ -5,30 +5,68 @@ import java.util.ArrayList;
 import com.googlecode.toolkits.stardict.StarDict;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     
-    private String TAG = "MainActivity";
+    private String TAG = "MainActivity";    
+    private StarDict dict = null;
+    private ArrayList<String> wordList = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Log.i(TAG, Environment.getDataDirectory().getAbsolutePath());
-        StarDict dict = new StarDict("/data/data/net.phamngochai.beefnoodle/dictd_anh-viet");
+        //Log.i(TAG, Environment.getDataDirectory().getAbsolutePath());
+        //dict = new StarDict(Environment.getDataDirectory().getAbsolutePath() +"/net.phamngochai.beefnoodle/dictd_anh-viet");
+        if (dict == null) {
+            dict = new StarDict("/sdcard/data/net.phamngochai.beefnoodle/dictd_anh-viet");
+            wordList = dict.getWordList();
+        }
+    }
+    
+    public void searchForWord(View view) {
         StringBuffer sb = new StringBuffer();
-//        for (int i = 0; i < 1; i++) {            
-//            dict.getWord(i, sb);
-//            Log.i(TAG, "Word " + i + " is: " + sb.toString());
-//        }
-        ArrayList<String> wordList = dict.getWordList();
-        dict.getWord(wordList.indexOf("amazing"), sb);
-        Log.i(TAG, "amazing means" + sb.toString());
-        dict.getWord(wordList.indexOf("wow"), sb);
-        Log.i(TAG, "wow means" + sb.toString());
+        EditText wordText = (EditText)findViewById(R.id.Word);
+        Log.i(TAG, "Looking for: " + wordText.getText().toString());
+        int wordPos = wordList.indexOf(wordText.getText().toString());
+        if (wordPos == -1) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage("No such word");
+            alert.show();
+        } else {
+            dict.getWord(wordPos, sb);
+            TextView meaning = (TextView)findViewById(R.id.Meanning);
+            meaning.setText(sb.toString());
+            meaning.setMovementMethod(new ScrollingMovementMethod());            
+        }
+        
+    }
+    
+    private void checkStorage() {
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // We can read and write the media
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // We can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Something else is wrong. It may be one of many other states, but all we need
+            //  to know is we can neither read nor write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }        
     }
     
 }

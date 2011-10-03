52de1d561fb4fd7caf9939584d1d37d7b98461d9
ngochai <pngochai@yahoo.com>
2011-10-03 20:03:38 +0800

    unregister broadcast

--- src/net/phamngochai/beefnoodle/MainActivity.java
+++ src/net/phamngochai/beefnoodle/MainActivity.java
@@ -6,6 +6,12 @@ import net.phamngochai.beefnoodle.dictionary.SunDict;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.method.ScrollingMovementMethod;
@@ -20,16 +26,75 @@ public class MainActivity extends Activity {
     private SunDict dict = null;
     private ArrayList<String> wordList = null;
     
+    boolean mExternalStorageAvailable = false;
+    boolean mExternalStorageWriteable = false;
+    
+    BroadcastReceiver mediaActionReceiver;
+    SharedPreferences settings;
+    
+    public static final String DICT_LOCATION = "DICT_LOCATION";
+    public static final String STORAGE_LOCATION = "STORAGE_LOCATION";
+
+    private String dictLoc = null;
+    private String storeLoc = null;
+    
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         //Log.i(TAG, Environment.getDataDirectory().getAbsolutePath());
         //dict = new StarDict(Environment.getDataDirectory().getAbsolutePath() +"/net.phamngochai.beefnoodle/dictd_anh-viet");
-        if (dict == null) {
-            dict = new SunDict("/sdcard/data/net.phamngochai.beefnoodle/dictd_anh-viet");
-            wordList = dict.getWordList();
+        settings = getPreferences(0);
+        storeLoc = settings.getString(STORAGE_LOCATION, null);
+        Log.d(TAG, "storeLoc: " + storeLoc);
+        if (storeLoc == null) {
+        	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        	builder.setMessage("You don't have any dictionary, do you want to download some from internet?")
+        	       .setCancelable(false)
+        	       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        	           public void onClick(DialogInterface dialog, int id) {
+        	                
+        	           }
+        	       })
+        	       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+        	           public void onClick(DialogInterface dialog, int id) {
+        	                MainActivity.this.finish();
+        	           }
+        	       });
+        	AlertDialog alert = builder.create();
+        	alert.show();
+        	Log.d(TAG, "AlertDialog created");
         }
+        
+        
+        dictLoc = settings.getString(DICT_LOCATION, null);
+        if (dictLoc == null) {
+        	
+        }
+        
+        
+        checkStorage();
+        IntentFilter filterMediaMounted = new IntentFilter (Intent.ACTION_MEDIA_MOUNTED);
+        IntentFilter filterMediaRemoved = new IntentFilter (Intent.ACTION_MEDIA_REMOVED);        
+        mediaActionReceiver  = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent intent) {
+            	checkStorage();
+            }
+        };        
+        registerReceiver(this.mediaActionReceiver, new IntentFilter(filterMediaMounted));
+        registerReceiver(this.mediaActionReceiver, new IntentFilter(filterMediaRemoved));
+        
+//        if (dict == null) {
+//            dict = new SunDict("/sdcard/data/net.phamngochai.beefnoodle/dictd_anh-viet");
+//            wordList = dict.getWordList();
+//        }
+    }
+    
+    @Override
+    public void onStop() {
+    	unregisterReceiver(mediaActionReceiver);
     }
     
     public void searchForWord(View view) {
@@ -51,10 +116,7 @@ public class MainActivity extends Activity {
     }
     
     private void checkStorage() {
-        boolean mExternalStorageAvailable = false;
-        boolean mExternalStorageWriteable = false;
         String state = Environment.getExternalStorageState();
-
         if (Environment.MEDIA_MOUNTED.equals(state)) {
             // We can read and write the media
             mExternalStorageAvailable = mExternalStorageWriteable = true;
@@ -64,9 +126,9 @@ public class MainActivity extends Activity {
             mExternalStorageWriteable = false;
         } else {
             // Something else is wrong. It may be one of many other states, but all we need
-            //  to know is we can neither read nor write
+            // to know is we can neither read nor write
             mExternalStorageAvailable = mExternalStorageWriteable = false;
-        }        
+        }
     }
     
 }

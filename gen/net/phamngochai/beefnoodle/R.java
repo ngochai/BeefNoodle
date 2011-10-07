af8bc9cf13196ac7d30393ceb258ad0909e9c4b4
ngochai <ngochai@dv4>
2011-10-07 12:57:53 +0800

    Fix list alignment

--- gen/net/phamngochai/beefnoodle/R.java
+++ gen/net/phamngochai/beefnoodle/R.java
@@ -14,18 +14,20 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Controls=0x7f050004;
-        public static final int Meanning=0x7f050007;
-        public static final int Search=0x7f050006;
-        public static final int Word=0x7f050005;
-        public static final int description=0x7f050003;
-        public static final int downloadDictionayList=0x7f050000;
-        public static final int list=0x7f050001;
-        public static final int name=0x7f050002;
+        public static final int Controls=0x7f050005;
+        public static final int Meanning=0x7f050008;
+        public static final int Search=0x7f050007;
+        public static final int Word=0x7f050006;
+        public static final int dictionary_description=0x7f050002;
+        public static final int dictionary_license=0x7f050003;
+        public static final int dictionary_name=0x7f050001;
+        public static final int downloadDictionayList=0x7f050004;
+        public static final int linearLayout1=0x7f050000;
     }
     public static final class layout {
-        public static final int downloaddictionary=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int dictionarylistrow=0x7f030000;
+        public static final int downloaddictionary=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         /**      <string name="hello">Hello World, MainActivity!</string> 
